74d41f5ff08c65fc2de0aae4f729691a9233f93d
Hamim Rahman <hamimr3823@gmail.com>
2023-10-29 13:50:28 +0600

    Server Creating api completed

---
+++ lib/current-profile.ts
@@ -0,0 +1,19 @@
+import {auth} from "@clerk/nextjs"
+
+import { db } from "@/lib/db"
+
+export const CurrentProfile =async () => {
+    const {userId} = auth()
+
+    if(!userId){
+        return null
+    }
+
+    const profile = await db.profile.findUnique({
+        where:{
+            userId
+        }
+    })
+
+    return profile;
+}
