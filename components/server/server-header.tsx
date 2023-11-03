c19d4b89cbf6b3d5d37a29e83571f3182a7a3e08
Hamim Rahman <hamimr3823@gmail.com>
2023-11-03 10:56:41 +0600

    Server Leave for members & Delete for Admin Done

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -58,13 +58,13 @@ export const ServerHeader = ({
                     <DropdownMenuSeparator/>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="px-3 text-rose-500 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={()=>onOpen("deleteServer",{server})} className="px-3 text-rose-500 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash2 className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
                 )}
                 {!isAdmin && (
-                    <DropdownMenuItem className="px-3 text-rose-500 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem onClick={()=> onOpen("leaveServer",{server})} className="px-3 text-rose-500 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto"/>
                     </DropdownMenuItem>
