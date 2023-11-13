f2b3845819bc93a35052cca9f98288f011bf5ebb
Hamim Rahman <hamimr3823@gmail.com>
2023-11-13 22:43:25 +0600

    Added Emoji Bar Successfully

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -16,6 +16,8 @@ import {
 import { Input } from "../ui/input"
 import { Plus, Smile } from "lucide-react"
 import { useModal } from "@/hooks/use-modal-store"
+import { EmojiPicker } from "../emoji-picker"
+import { useRouter } from "next/navigation"
 
 interface ChatInputProps{
     apiUrl: string
@@ -36,6 +38,7 @@ const ChatInput = ({
 }:ChatInputProps) => {
 
     const {onOpen} = useModal()
+    const router = useRouter()
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -53,6 +56,8 @@ const ChatInput = ({
             })
 
             await axios.post(url, values)
+            form.reset()
+            router.refresh()
         } catch (error) {
             console.log(error)
         }
@@ -73,7 +78,9 @@ const ChatInput = ({
                                     </button>
                                     <Input disabled={isLoading} className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200" placeholder={`Message ${type === 'conversation'?name: "#" + name}`} {...field}/>
                                     <div className="absolute top-7 right-8">
-                                        <Smile/>
+                                        <EmojiPicker
+                                            onChange={(emoji:string)=>field.onChange(`${field.value} ${emoji}`)}
+                                        />
                                     </div>
                                 </div>
                             </FormControl>
